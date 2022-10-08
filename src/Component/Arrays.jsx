5f84dc621a79303cf9c49e57eb764f07d345b5d0
Yeraf <yerafarceguerrero9@gmail.com>
2022-10-08 14:57:12 -0600

    commit

---
+++ src/Component/Arrays.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Arrays() {
+  const users = [
+    {
+      id: "1",
+      name: "Paul",
+      img: "https://robohash.org/2600:6c5e:357f:e274:dd4:78bb:b35f:924d.png",
+    },
+    {
+      id: "2",
+      name: "Marrie",
+      img: "https://robohash.org/user3",
+    },
+    {
+      id: "3",
+      name: "Sab",
+      img: "https://robohash.org/user7",
+    },
+  ];
+
+  return <div key={1}> </div>;
+}
+
+export default Arrays;
